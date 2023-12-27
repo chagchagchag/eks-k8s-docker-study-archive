7a4ea27e8d6f951972cb27c83496549c6a97d5a7
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-28 07:20:20 +0900

    jib spring boot prj

---
+++ k8s_jib_demo/src/main/java/dev/k8s/gogogo/k8s_jib_demo/K8sJibDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.k8s.gogogo.k8s_jib_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sJibDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sJibDemoApplication.class, args);
+	}
+
+}
---
+++ k8s_jib_demo/src/test/java/dev/k8s/gogogo/k8s_jib_demo/K8sJibDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.k8s.gogogo.k8s_jib_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sJibDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
