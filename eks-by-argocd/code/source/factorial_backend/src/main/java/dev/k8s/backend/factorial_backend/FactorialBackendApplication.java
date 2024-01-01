2ec2830dd35d85ae3a33be8fc9851ea565bd8f39
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-02 00:39:27 +0900

    eks with argocd

---
+++ eks-by-argocd/code/source/factorial_backend/src/main/java/dev/k8s/backend/factorial_backend/FactorialBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.k8s.backend.factorial_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorialBackendApplication.class, args);
+	}
+
+}
---
+++ eks-by-argocd/code/source/factorial_backend/src/test/java/dev/k8s/backend/factorial_backend/FactorialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.k8s.backend.factorial_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
