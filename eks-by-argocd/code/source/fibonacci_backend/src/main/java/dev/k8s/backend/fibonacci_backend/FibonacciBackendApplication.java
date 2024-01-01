ba74e4e743bbba935ff4b1dc92f9dfb3242a1877
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-02 00:42:21 +0900

    eks with argocd

---
+++ eks-by-argocd/code/source/fibonacci_backend/src/main/java/dev/k8s/backend/fibonacci_backend/FibonacciBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.k8s.backend.fibonacci_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonacciBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibonacciBackendApplication.class, args);
+	}
+
+}
---
+++ eks-by-argocd/code/source/fibonacci_backend/src/test/java/dev/k8s/backend/fibonacci_backend/FibonacciBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.k8s.backend.fibonacci_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonacciBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
