d003cb7ff1b24fd3c8e8a9c6f5910682a01d0966
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-04 02:44:54 +0900

    eks with argocd

--- eks-by-argocd/code/source/fibonacci_backend_web/src/main/java/dev/k8s/backend/fibonacci_backend_web/application/FibonacciService.java
+++ eks-by-argocd/code/source/fibonacci_backend_web/src/main/java/dev/k8s/backend/fibonacci_backend_web/application/FibonacciService.java
@@ -1,15 +1,26 @@
 package dev.k8s.backend.fibonacci_backend_web.application;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class FibonacciService {
 
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public FibonacciService(StringRedisTemplate stringRedisTemplate){
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
     private final Map<Integer, BigDecimal> dp = new HashMap<>();
+    private static final String QUEUE_HASH_KEY = "fibonacci:task-queue";
+    private static final String SET_HASH_KEY = "fibonacci:result-set";
 
     public BigDecimal getFibonacci(int number) {
         if(number == 0) return BigDecimal.ZERO;
@@ -21,6 +32,24 @@ public class FibonacciService {
             dp.put(number, getFibonacci(number-2).add(getFibonacci(number-1)));
 
             return dp.get(number);
+        }
+    }
+
+    // **TODO** : 'fibonacci-backend-batch' 로 아래 기능을 이관 예정.
+    @Scheduled(fixedDelay = 1000L)
+    public void scheduledCalculateFibonacci(){
+        if(Boolean.TRUE.equals(stringRedisTemplate.hasKey(QUEUE_HASH_KEY))){
+            Optional
+                .ofNullable(stringRedisTemplate.opsForSet().pop(QUEUE_HASH_KEY))
+                .ifPresent(cachedRequest -> {
+                    BigDecimal result = getFibonacci(Integer.parseInt(cachedRequest));
+                    stringRedisTemplate.opsForHash()
+                            .put(
+                                SET_HASH_KEY,
+                                cachedRequest,
+                                result.toPlainString()
+                            );
+                });
         }
     }
 
