7f6abcb558c6749fb1ad2c5b2988ef72915e7cd2
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-04 01:39:15 +0900

    eks with argocd

--- eks-by-argocd/code/source/fibonacci_backend_web/k8s/sh/0-setting-revert.sh
+++ eks-by-argocd/code/source/fibonacci_backend_web/k8s/sh/0-setting-revert.sh
@@ -1,9 +1,14 @@
-kubectl delete -f ../local-storage.yml
-kubectl delete -f ../fibonacci-namespace.yml
-kind delete cluster --name fibonacci-cluster
+echo "[delete] storage, namespace"
+kubectl delete -f ../fibonacci-common/local-storage.yml
+kubectl delete -f ../fibonacci-common/fibonacci-namespace.yml
 
-echo "--- redis setting (delete) ---"
-echo "kubectl delete -f ../redis-pod.yml"
-kubectl delete -f ../redis-pod.yml
-echo "kubectl apply -f ../redis-service.yml"
-kubectl delete -f ../redis-service.yml
+echo ""
+echo "[delete] redis setting ---"
+echo "kubectl delete -f ../fibonacci-common/redis-pod.yml"
+kubectl delete -f ../fibonacci-common/redis-pod.yml
+echo "kubectl delete -f ../fibonacci-common/redis-service.yml"
+kubectl delete -f ../fibonacci-common/redis-service.yml
+
+echo ""
+echo "[delete] cluster"
+kind delete cluster --name fibonacci-cluster
--- eks-by-argocd/code/source/fibonacci_backend_web/k8s/sh/1-application-revert.sh
+++ eks-by-argocd/code/source/fibonacci_backend_web/k8s/sh/1-application-revert.sh
@@ -1,6 +1,15 @@
-kubectl delete -f ../fibonacci-cache-deploy.yml
-kubectl delete -f ../fibonacci-cache-ingress.yml
-kubectl delete -f ../fibonacci-cache-service.yml
-kubectl delete -f ../fibonacci-cache-config.yml
-kubectl delete -f ../fibonacci-cache-log-pvc.yml
+echo ""
+echo "[delete] fibonacci-backend-web"
+kubectl delete -f ../fibonacci-backend-web/fibonacci-web-service.yml
+kubectl delete -f ../fibonacci-backend-web/fibonacci-web-deploy.yml
+
+echo ""
+echo "[delete] fibonacci-backend-cache"
+kubectl delete -f ../fibonacci-backend-cache/fibonacci-cache-deploy.yml
+kubectl delete -f ../fibonacci-backend-cache/fibonacci-cache-ingress.yml
+kubectl delete -f ../fibonacci-backend-cache/fibonacci-cache-service.yml
+kubectl delete -f ../fibonacci-backend-cache/fibonacci-cache-config.yml
+kubectl delete -f ../fibonacci-backend-cache/fibonacci-cache-log-pvc.yml
+
+echo "[delete] secret"
 kubectl -n fibonacci delete secret fibonacci-cache-secret
