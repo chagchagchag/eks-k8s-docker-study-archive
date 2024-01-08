c0c17eb40f4de80ca451730edfddbbbae360e724
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-08 11:40:10 +0900

    2024.01

--- local-k8s-kind/yml/setup-argocd.sh
+++ local-k8s-kind/yml/setup-argocd.sh
@@ -7,22 +7,6 @@ echo "[install] kubectl apply -f argoprj/argo-cd/.../install.yaml"
 kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 echo ""
-echo "[setup] ingress (argocd-ingress)"
-kubectl -n argocd apply -f argocd-ingress.yml
-
-# echo ""
-# echo "[setup] nodeport (argocd-server-nodeport)"
-# kubectl apply -f argocd-server-nodeport.yml
-
-# echo ""
-# echo "[setup] cluster ip (argocd-service-clusterip)"
-# kubectl apply -f argocd-service-clusterip.yml
-
-# echo ""
-# echo "[setup] ingress (argocd-server-ingress)"
-# kubectl -n argocd apply -f argocd-ingress-only.yml
-
-echo ""
 echo "[configure] --insecure configure"
 kubectl -n argocd patch deployment argocd-server --type json -p='[{"op":"replace","path":"/spec/template/spec/containers/0/args","value":["/usr/local/bin/argocd-server","--insecure"]}]'
 
@@ -31,5 +15,18 @@ echo "[status] kubectl -n argocd get all"
 kubectl -n argocd get all
 
 echo ""
-echo "[password!!!] your password
+echo "wait(45s) ... "
+sleep 45
+
+echo ""
+echo "[password!!!] your password"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+echo ""
+echo ""
+echo "wait(45s) ... "
+sleep 45
+
+echo ""
+echo "[setup] ingress (argocd-ingress)"
+kubectl apply -f argocd-ingress.yml
