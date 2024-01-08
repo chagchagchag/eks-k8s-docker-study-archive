d4b173a93b98d20ae5ff163aeb7c0b9d6569e63a
chagchagchag <chagchagchag.dev@gmail.com>
2024-01-08 20:01:11 +0900

    2024.01

--- eks-by-argocd/code/source/fibonacci_backend_cache/k8s/sh/1-application-revert.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete -f ../fibonacci-cache-deploy.yml
-kubectl delete -f ../fibonacci-cache-ingress.yml
-kubectl delete -f ../fibonacci-cache-service.yml
-kubectl delete -f ../fibonacci-cache-config.yml
-kubectl delete -f ../fibonacci-cache-log-pvc.yml
-kubectl -n fibonacci delete secret fibonacci-cache-secret
--- eks-by-argocd/code/source/fibonacci_backend_cache/k8s/sh/start.sh
+++
@@ -1,2 +0,0 @@
-source 0-setting.sh
-source 1-application.sh
